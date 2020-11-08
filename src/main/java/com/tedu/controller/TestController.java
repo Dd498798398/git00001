c2bf9fc962746114dd70258f6c046e69ec117fdb
zhang <498798398@qq.com>
2020-11-08 19:30:27 +0800

    bbb

---
+++ src/main/java/com/tedu/controller/TestController.java
@@ -0,0 +1,19 @@
+package com.tedu.controller;
+
+import org.junit.Test;
+
+/**
+ * @description ：
+ * @Date : 2020/11/8，19:26
+ * @Author : Zhang Laoshi
+ */
+public class TestController {
+
+    @Test
+    public void test() {
+        System.out.println("123");
+        System.out.println("bbb");
+    }
+
+
+}
